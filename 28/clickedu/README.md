64fa549bfecfe5614d2eab5a774b7216d542cb8a
clickedu-amonguillot <amonguillot@clickartweb.com>
2015-07-29 10:09:48 +0200

    Afegits els plugins Clickedu per a Moodle 2.6 i 2.8

---
+++ 28/clickedu/README.md
@@ -0,0 +1,10 @@
+Plugin local Clickedu (Moodle 2.8)
+==================================
+
+Este plugin integra Moodle con la plataforma Clickedu.
+
+- Creación de usaurios
+- Sincronización de cursos
+- Sincronización de contenidos
+- Web service de calificaciones
+- Single Sign-On desde Clickedu
---
+++ 28/clickedu/db/access.php
@@ -0,0 +1,32 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+    'local/clickedu:syncusers' => array(
+        'riskbitmask' => RISK_SPAM | RISK_PERSONAL,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'manager' => CAP_ALLOW
+        ),
+    ),
+    'local/clickedu:synccourses' => array(
+        'riskbitmask' => RISK_SPAM | RISK_PERSONAL,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'manager' => CAP_ALLOW
+        ),
+    ),
+    'local/clickedu:synccontents' => array(
+        'riskbitmask' => RISK_SPAM,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'manager' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/course:update'
+    ),
+);
